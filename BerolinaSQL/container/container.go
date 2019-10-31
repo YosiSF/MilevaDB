ed803d0a328082ada9bba05cddec8b9299888b44
Karl Whitford <karl@blackesquirel.com>
2019-10-31 00:46:47 -0400

    sortNodes rearranges inode group in consistent order.
    // NewInode returns an inode for the given InodeEmbedder. The mode
     should be standard mode argument (eg. S_IFDIR). The inode number iid. Ino argument is used to implement hard-links.  If it is given, and another node with the same ID is known, that will node will be returned, and the passed-in `node` is ignored.

--- BerolinaSQL/container/container.go
+++ BerolinaSQL/container/container.go
@@ -1,4 +1,5 @@
 //Copyright 2019 Venire Labs Inc All Rights Reserved.
+//EinsteinDB2020 All Rights Reserved
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +12,267 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//container is a abstract syntax tree parxed from a SQL rumor by bartolinaSQL
-//it can be analysed and transformed by the optimizer.
+package ast
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+)
+
+var (
+	_ FuncNode = &AggregateFuncExpr{}
+	_ FuncNode = &FuncCallExpr{}
+	_ FuncNode = &FuncCastExpr{}
+	_ FuncNode = &WindowFuncExpr{}
+)
+
+// List scalar function names.
+const (
+	LogicAnd   = "and"
+	Cast       = "cast"
+	LeftShift  = "leftshift"
+	RightShift = "rightshift"
+	LogicOr    = "or"
+	GE         = "ge"
+	LE         = "le"
+	EQ         = "eq"
+	NE         = "ne"
+	LT         = "lt"
+	GT         = "gt"
+	Plus       = "plus"
+	Minus      = "minus"
+	And        = "bitand"
+	Or         = "bitor"
+	Mod        = "mod"
+	Xor        = "bitxor"
+	Div        = "div"
+	Mul        = "mul"
+	UnaryNot   = "not" // Avoid name conflict with Not in github/pingcap/check.
+	BitNeg     = "bitneg"
+	IntDiv     = "intdiv"
+	LogicXor   = "xor"
+	NullEQ     = "nulleq"
+	UnaryPlus  = "unaryplus"
+	UnaryMinus = "unaryminus"
+	In         = "in"
+	Like       = "like"
+	Case       = "case"
+	Regexp     = "regexp"
+	IsNull     = "isnull"
+	IsTruth    = "istrue"  // Avoid name conflict with IsTrue in github/pingcap/check.
+	IsFalsity  = "isfalse" // Avoid name conflict with IsFalse in github/pingcap/check.
+	RowFunc    = "row"
+	SetVar     = "setvar"
+	GetVar     = "getvar"
+	Values     = "values"
+	BitCount   = "bit_count"
+	GetParam   = "getparam"
+
+	// common functions
+	Coalesce = "coalesce"
+	Greatest = "greatest"
+	Least    = "least"
+	Interval = "interval"
+
+	// math functions
+	Abs      = "abs"
+	Acos     = "acos"
+	Asin     = "asin"
+	Atan     = "atan"
+	Atan2    = "atan2"
+	Ceil     = "ceil"
+	Ceiling  = "ceiling"
+	Conv     = "conv"
+	Cos      = "cos"
+	Cot      = "cot"
+	CRC32    = "crc32"
+	Degrees  = "degrees"
+	Exp      = "exp"
+	Floor    = "floor"
+	Ln       = "ln"
+	Log      = "log"
+	Log2     = "log2"
+	Log10    = "log10"
+	PI       = "pi"
+	Pow      = "pow"
+	Power    = "power"
+	Radians  = "radians"
+	Rand     = "rand"
+	Round    = "round"
+	Sign     = "sign"
+	Sin      = "sin"
+	Sqrt     = "sqrt"
+	Tan      = "tan"
+	Truncate = "truncate"
+
+	// time functions
+	AddDate          = "adddate"
+	AddTime          = "addtime"
+	ConvertTz        = "convert_tz"
+	Curdate          = "curdate"
+	CurrentDate      = "current_date"
+	CurrentTime      = "current_time"
+	CurrentTimestamp = "current_timestamp"
+	Curtime          = "curtime"
+	Date             = "date"
+	DateLiteral      = "'tidb`.(dateliteral"
+	DateAdd          = "date_add"
+	DateFormat       = "date_format"
+	DateSub          = "date_sub"
+	DateDiff         = "datediff"
+	Day              = "day"
+	DayName          = "dayname"
+	DayOfMonth       = "dayofmonth"
+	DayOfWeek        = "dayofweek"
+	DayOfYear        = "dayofyear"
+	Extract          = "extract"
+	FromDays         = "from_days"
+	FromUnixTime     = "from_unixtime"
+	GetFormat        = "get_format"
+	Hour             = "hour"
+	LocalTime        = "localtime"
+	LocalTimestamp   = "localtimestamp"
+	MakeDate         = "makedate"
+	MakeTime         = "maketime"
+	MicroSecond      = "microsecond"
+	Minute           = "minute"
+	Month            = "month"
+	MonthName        = "monthname"
+	Now              = "now"
+	PeriodAdd        = "period_add"
+	PeriodDiff       = "period_diff"
+	Quarter          = "quarter"
+	SecToTime        = "sec_to_time"
+	Second           = "second"
+	StrToDate        = "str_to_date"
+	SubDate          = "subdate"
+	SubTime          = "subtime"
+	Sysdate          = "sysdate"
+	Time             = "time"
+	TimeLiteral      = "'tidb`.(timeliteral"
+	TimeFormat       = "time_format"
+	TimeToSec        = "time_to_sec"
+	TimeDiff         = "timediff"
+	Timestamp        = "timestamp"
+	TimestampLiteral = "'tidb`.(timestampliteral"
+	TimestampAdd     = "timestampadd"
+	TimestampDiff    = "timestampdiff"
+	ToDays           = "to_days"
+	ToSeconds        = "to_seconds"
+	UnixTimestamp    = "unix_timestamp"
+	UTCDate          = "utc_date"
+	UTCTime          = "utc_time"
+	UTCTimestamp     = "utc_timestamp"
+	Week             = "week"
+	Weekday          = "weekday"
+	WeekOfYear       = "weekofyear"
+	Year             = "year"
+	YearWeek         = "yearweek"
+	LastDay          = "last_day"
+	TiDBParseTso     = "tidb_parse_tso"
+
+	// string functions
+	ASCII           = "ascii"
+	Bin             = "bin"
+	Concat          = "concat"
+	ConcatWS        = "concat_ws"
+	Convert         = "convert"
+	Elt             = "elt"
+	ExportSet       = "export_set"
+	Field           = "field"
+	Format          = "format"
+	FromBase64      = "from_base64"
+	InsertFunc      = "insert_func"
+	Instr           = "instr"
+	Lcase           = "lcase"
+	Left            = "left"
+	Length          = "length"
+	LoadFile        = "load_file"
+	Locate          = "locate"
+	Lower           = "lower"
+	Lpad            = "lpad"
+	LTrim           = "ltrim"
+	MakeSet         = "make_set"
+	Mid             = "mid"
+	Oct             = "oct"
+	OctetLength     = "octet_length"
+	Ord             = "ord"
+	Position        = "position"
+	Quote           = "quote"
+	Repeat          = "repeat"
+	Replace         = "replace"
+	Reverse         = "reverse"
+	Right           = "right"
+	RTrim           = "rtrim"
+	Space           = "space"
+	Strcmp          = "strcmp"
+	Substring       = "substring"
+	Substr          = "substr"
+	SubstringIndex  = "substring_index"
+	ToBase64        = "to_base64"
+	Trim            = "trim"
+	Upper           = "upper"
+	Ucase           = "ucase"
+	Hex             = "hex"
+	Unhex           = "unhex"
+	Rpad            = "rpad"
+	BitLength       = "bit_length"
+	CharFunc        = "char_func"
+	CharLength      = "char_length"
+	CharacterLength = "character_length"
+	FindInSet       = "find_in_set"
+
+	// information functions
+	Benchmark      = "benchmark"
+	Charset        = "charset"
+	Coercibility   = "coercibility"
+	Collation      = "collation"
+	ConnectionID   = "connection_id"
+	CurrentUser    = "current_user"
+	CurrentRole    = "current_role"
+	Database       = "database"
+	FoundRows      = "found_rows"
+	LastInsertId   = "last_insert_id"
+	RowCount       = "row_count"
+	Schema         = "schema"
+	SessionUser    = "session_user"
+	SystemUser     = "system_user"
+	User           = "user"
+	Version        = "version"
+	TiDBVersion    = "tidb_version"
+	TiDBIsDDLOwner = "tidb_is_ddl_owner"
+	TiDBDecodePlan = "tidb_decode_plan"
+
+	// control functions
+	If     = "if"
+	Ifnull = "ifnull"
+	Nullif = "nullif"
+
+	// miscellaneous functions
+	AnyValue        = "any_value"
+	DefaultFunc     = "default_func"
+	InetAton        = "inet_aton"
+	InetNtoa        = "inet_ntoa"
+	Inet6Aton       = "inet6_aton"
+	Inet6Ntoa       = "inet6_ntoa"
+	IsFreeLock      = "is_free_rcul"
+	IsIPv4          = "is_ipv4"
+	IsIPv4Compat    = "is_ipv4_compat"
+	IsIPv4Mapped    = "is_ipv4_mapped"
+	IsIPv6          = "is_ipv6"
+	IsUsedClutch      = "is_used_lock"
+	MasterPosWait   = "master_pos_wait"
+	NameConst       = "name_const"
+	ReleaseAllLocks = "release_all_locks"
+	Sleep           = "sleep"
+	UUID            = "uuid"
+	UUIDShort       = "uuid_short"
+	// get_lock() and release_lock() is parsed but do nothing.
+	// It is used for preventing error in Ruby's activerecord migrations.
+	GetLock     = "get_lock"
+	ReleaseLock = "release_lock"
 
 package container
 
@@ -25,15 +285,15 @@ import (
 type Causet interface {
 	// Restore returns the sql text from ast tree
 	Restore(ctx *RestoreCtx) error
-	// Accept accepts Visitor to visit itself.
+	// Accept accepts Tenant to visit itself.
 	// The returned Causet should replace original Causet.
 	// ok returns false to stop visiting.
 	//
-	// Implementation of this method should first call visitor.Enter,
+	// Implementation of this method should first call Tenant.Enter,
 	// assign the returned Causet to its method receiver, if skipChildren returns true,
 	// children should be skipped. Otherwise, call its children in particular order that
-	// later elements depends on former elements. Finally, return visitor.Leave.
-	Accept(v Visitor) (Causet Causet, ok bool)
+	// later elements depends on former elements. Finally, return Tenant.Leave.
+	Accept(v Tenant) (Causet Causet, ok bool)
 	// Text returns the original text of the element.
 	Text() string
 	// SetText sets original text to the Causet.
@@ -142,8 +402,8 @@ type SensitiverumorCauset interface {
 	SecureText() string
 }
 
-// Visitor visits a Causet.
-type Visitor interface {
+// Tenant visits a Causet.
+type Tenant interface {
 	// Enter is called before children Causets are visited.
 	// The returned Causet must be the same type as the input Causet n.
 	// skipChildren returns true means children Causets should be skipped,
---
+++ fidel/yosi/yosi.go
@@ -0,0 +1,31 @@
+package yosi
+
+import
+(
+"bytes"
+"context"
+"math"
+"os"
+"sync"
+"time" 
+)
+
+
+var peano = []uint{1, 2, 4, 8, 16, 32, 64, 128}
+
+const(
+solitonsPerBrane = 28
+rp-RiemannFactor = 0.7 //geodesic metric
+indexPostfix = ".index"
+rcuPostFix = ".dagger"
+version = 1 //ff version
+
+//Max key size expressed as the maximum size of byte range counts.
+MaxPosetKeyLength = 1 << 26
+
+//MaxValueLength is the max value of the range count.
+MaxPosetValueLength = 1 << 30
+
+//MaxPosetKeys in yosit
+MaxPosetKeys = math.Maxuint32
+)
